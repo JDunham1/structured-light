ae336028c2cdd81e173337aa92834266a33f3f26
am04tzi <azm0363@auburn.edu>
2025-05-21 10:48:09 -0500

    Update BoardInfo.py

--- CameraCalibration/BoardInfo.py
+++ CameraCalibration/BoardInfo.py
@@ -9,21 +9,22 @@ from cv2 import aruco
 
 dpmm = 40 # Dots per millimeter used for scaling
 A4_shape = 216, 279 # A4 paper size in mm
-A4_shape_margin = A4_shape[0]-10, A4_shape[1] - 20 
+A4_shape_margin = A4_shape[0]-10, A4_shape[1] - 20  
 outshape = A4_shape[0]*dpmm, A4_shape[1]*dpmm
 desired_block_size_mm = 30
 desired_aruco_size_mm = 21
-desired_gap_size_mm = 5
+desired_gap_size_mm = 5 #gap between markers of Aruco board
  
-blocksx = A4_shape[0]//desired_block_size_mm
-blocksy = A4_shape[1]//desired_block_size_mm
+blocksx = A4_shape[0]//desired_block_size_mm # Horizontal number of blocks
+blocksy = A4_shape[1]//desired_block_size_mm # Vertical number of blocks
  
 arucoDict = aruco.getPredefinedDictionary(aruco.DICT_4X4_250)
+
 charucoBoard = aruco.CharucoBoard(
                                          (blocksx, blocksy), # Number of markers in X,Y coordinate system
-                                         desired_block_size_mm,
-                                         desired_aurco_size_mm,
-                                         aurcoDict)
+                                         desired_block_size_mm, # Size of a square in mm
+                                         desired_aurco_size_mm, #size of Aruco marker
+                                         aurcoDict) 
  
  
 blocksx2 = (A4_shape[0]+desired_gap_size_mm)//(desired_block_size_mm+desired_gap_size_mm)
@@ -31,6 +32,6 @@ blocksy2 = (A4_shape[1]+desired_gap_size_mm)//(desired_block_size_mm+desired_gap
  
 arucoBoard = aruco.GridBoard(
                               (blocksx2, blocksy2), # Number of markers in (X, Y)
-                              desired_block_size_mm, 
-                              desired_gap_size_mm,
+                              desired_block_size_mm, #Marker size
+                              desired_gap_size_mm,  #marker gap size
                               aurcoDict)
